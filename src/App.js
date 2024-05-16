ea1621a1d595c80fbe1a8e0eabf087269a8a9cbf
Ashmit Sinha <ashmitsinha15@gmail.com>
2024-05-17 01:09:46 +0530

    4

--- src/App.js
+++ src/App.js
@@ -44,10 +44,11 @@ function App() {
       <Router>
       <Navbar title="Word Counter" about="About" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
+      {/* <TextForm heading="Enter text to analyze below" mode={mode} showAlert={showAlert} /> */}
       <div className="container my-5">
         <Routes>
+            <Route index element={<TextForm heading="Enter text to analyze below" mode={mode} showAlert={showAlert} />}/>
             <Route exact path="/about" element={<About mode={mode}/>} />
-            <Route exact path="/" element={<TextForm heading="Enter text to analyze below" mode={mode} showAlert={showAlert} />}/>
         </Routes>  
       </div>
       </Router>
